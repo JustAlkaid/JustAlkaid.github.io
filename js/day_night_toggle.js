11dee343392ed4e970f47c86f673be94457a0406
Alkaid <72213408+JustAlkaid@users.noreply.github.com>
2024-11-16 22:25:10 +0800

    completely refactored nav.less

--- js/day_night_toggle.js
+++ js/day_night_toggle.js
@@ -2,6 +2,7 @@ document.getElementById('themetoggleday').addEventListener('click', function() {
     document.body.classList.remove('night');
     document.body.classList.add('day');
 });
+
 document.getElementById('themetogglenight').addEventListener('click', function() {
     document.body.classList.remove('day');
     document.body.classList.add('night');
