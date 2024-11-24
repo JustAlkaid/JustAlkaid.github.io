a020f8fcf4fca6d7aa2d512cbbdc15ce779a24ec
Alkaid <72213408+JustAlkaid@users.noreply.github.com>
2024-11-24 23:48:22 +0800

    complete canvas-js as the first treasurebox item

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,8 +1,8 @@
 document.addEventListener('mousemove', function(e) {
     const toggleContainer = document.getElementById('toggle-container');
     if (e.clientX < 100 && e.clientY < 100) {
-        toggleContainer.style.opacity = '1';
+        toggleContainer.classList.add('show');
     } else {
-        toggleContainer.style.opacity = '0';
+        toggleContainer.classList.remove('show');
     }
 });
